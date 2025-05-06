88bc72e4c47164a22857e665db0d65a78c81268b
Alen Eminovic <emal24na@student.ju.se>
2025-05-06 21:49:17 +0200

    Added loadData() back

--- js/api.js
+++ js/api.js
@@ -1,3 +1,18 @@
+// async function loadData() {
+//   const response = await fetch(
+//     "https://en.wikipedia.org/api/rest_v1/page/summary/Matcha"
+//   );
+//   const json = await response.json();
+
+//   const wikipediaUrl = json.title;
+
+//   const headingElement = document.createElement("h1");
+//   headingElement.innerHTML = wikipediaUrl;
+
+//   const bodyElement = document.querySelector("body");
+//   bodyElement.appendChild(headingElement);
+// }
+
 async function loadData() {
   const response = await fetch(
     "https://en.wikipedia.org/api/rest_v1/page/summary/Matcha"
@@ -29,3 +44,28 @@ async function loadMatcha() {
 
   renderMatcha();
 }
+
+function createMatchaElement(data) {
+  const card = document.createElement("div");
+  card.classList.add("matcha-card");
+
+  card.innerHTML = `
+    <img src="${data.thumbnail?.source}" alt="${data.title}" class="tip-image" />
+    <figure>
+      <h1>${data.title}</h1>
+      <h2>${data.description}</h2>
+      <h2>${data.extract}</h2>
+    </figure>
+  `;
+
+  return card;
+}
+
+function renderMatcha() {
+  matchaContainer.innerHTML = "";
+
+  const card = createMatchaElement(matchaData);
+  matchaContainer.appendChild(card);
+}
+
+loadMatcha();
